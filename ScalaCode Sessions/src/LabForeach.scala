6eca5e6322a4806e02b428b126f91bf9e6cb5a6d
Sebastian <sevashe@gmail.com>
2024-02-29 14:14:08 +0600

    Update Scala Code

--- ScalaCode Sessions/src/LabForeach.scala
+++ ScalaCode Sessions/src/LabForeach.scala
@@ -1,6 +1,6 @@
 object LabForeach {
   def main(args: Array[String]): Unit = {
-  var myArray = Array[Int](10,20,30,40,50,60,70,67,84,34,34)
+  var myArray = Array[Int](10,53,42,53,8,7,16,18,21,65)
 
 //    for item <- myArray do {
 //      if (item >= 40) {
@@ -8,6 +8,7 @@ object LabForeach {
 //      }
 //    }
 
-    myArray.filter(item => item >= 40).foreach(item => println(item))
+    var array = myArray.filter(item => item < 20)
+    array.foreach()
   }
 }
